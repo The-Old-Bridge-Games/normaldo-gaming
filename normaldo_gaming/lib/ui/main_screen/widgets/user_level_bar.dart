e0c832a82fba82056e33db3e0af1bcf84e8c1033
Sovetsk1y <39431565+Sovetsk1y@users.noreply.github.com>
2023-10-27 19:48:25 +0300

    WIP

--- normaldo_gaming/lib/ui/main_screen/widgets/user_level_bar.dart
+++ normaldo_gaming/lib/ui/main_screen/widgets/user_level_bar.dart
@@ -10,11 +10,15 @@ class UserLevelBar extends StatefulWidget {
   const UserLevelBar({
     required this.levelManager,
     required this.barWidth,
+    this.rankStyle,
+    this.iconSize,
     super.key,
   });
 
   final LevelManager levelManager;
   final double barWidth;
+  final TextStyle? rankStyle;
+  final double? iconSize;
 
   @override
   State<UserLevelBar> createState() => _LevelBarState();
@@ -24,85 +28,91 @@ class _LevelBarState extends State<UserLevelBar> {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    return Row(
+    return Column(
       mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
       children: [
-        Image.asset(
-          'assets/images/star.png',
-          fit: BoxFit.cover,
-          height: 30,
-          width: 30,
-        ),
-        const SizedBox(width: 8),
-        BlocBuilder<UserCubit, UserState>(
-          builder: (context, state) => Text(
-            '${state.user.level}',
-            style: textTheme.displayMedium,
-          ),
-        ),
-        const SizedBox(width: 16),
-        BlocBuilder<UserCubit, UserState>(builder: (context, state) {
-          return Column(
-            children: [
-              Center(child: RankLabel()),
-              const SizedBox(height: 8),
-              Stack(
-                children: [
-                  Container(
-                    width: widget.barWidth,
-                    height: 25,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[600],
-                      borderRadius: BorderRadius.circular(2.0),
-                    ),
-                  ),
-                  AnimatedContainer(
-                    duration: const Duration(seconds: 1),
-                    curve: Curves.fastLinearToSlowEaseIn,
-                    decoration: BoxDecoration(
-                      color: NGTheme.orange1,
-                      borderRadius: BorderRadius.circular(2.0),
-                    ),
-                    height: 25,
-                    width: widget.levelManager.isMaxLevel(state.user)
-                        ? widget.barWidth
-                        : widget.barWidth /
-                            widget.levelManager.nextLevelExp(state.user) *
-                            state.user.exp,
-                  ),
-                  Positioned(
-                    left: 8,
-                    right: 8,
-                    bottom: 2,
-                    child: Text(
-                      widget.levelManager.isMaxLevel(state.user)
-                          ? 'max level'
-                          : '${state.user.exp}/${widget.levelManager.nextLevelExp(state.user)}',
-                      textAlign: TextAlign.center,
-                      style: textTheme.displaySmall,
-                    ),
-                  )
-                ],
-              ),
-            ],
-          );
-        }),
-        Visibility(
-          visible: false,
-          child: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: BouncingButton(
-              child: const Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 32,
-              ),
-              onPressed: () {
-                context.read<UserCubit>().addExp(30);
-              },
+        RankLabel(style: widget.rankStyle),
+        const SizedBox(height: 8),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Image.asset(
+              'assets/images/star.png',
+              fit: BoxFit.cover,
+              height: widget.iconSize ?? 30,
+              width: widget.iconSize ?? 30,
             ),
-          ),
+            const SizedBox(width: 8),
+            BlocBuilder<UserCubit, UserState>(
+              builder: (context, state) => Text(
+                '${state.user.level}',
+                style: textTheme.displayMedium,
+              ),
+            ),
+            const SizedBox(width: 16),
+            BlocBuilder<UserCubit, UserState>(builder: (context, state) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        width: widget.barWidth,
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[600],
+                          borderRadius: BorderRadius.circular(2.0),
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(seconds: 1),
+                        curve: Curves.fastLinearToSlowEaseIn,
+                        decoration: BoxDecoration(
+                          color: NGTheme.orange1,
+                          borderRadius: BorderRadius.circular(2.0),
+                        ),
+                        height: 25,
+                        width: widget.levelManager.isMaxLevel(state.user)
+                            ? widget.barWidth
+                            : widget.barWidth /
+                                widget.levelManager.nextLevelExp(state.user) *
+                                state.user.exp,
+                      ),
+                      Positioned(
+                        left: 8,
+                        right: 8,
+                        bottom: 2,
+                        child: Text(
+                          widget.levelManager.isMaxLevel(state.user)
+                              ? 'max level'
+                              : '${state.user.exp}/${widget.levelManager.nextLevelExp(state.user)}',
+                          textAlign: TextAlign.center,
+                          style: textTheme.displaySmall,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              );
+            }),
+            Visibility(
+              visible: false,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: BouncingButton(
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 32,
+                  ),
+                  onPressed: () {
+                    context.read<UserCubit>().addExp(30);
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
--- normaldo_gaming/lib/ui/slot_machine/widgets/bid_widget.dart
+++ normaldo_gaming/lib/ui/slot_machine/widgets/bid_widget.dart
@@ -1,7 +1,7 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:normaldo_gaming/application/user/cubit/user_cubit.dart';
+import 'package:normaldo_gaming/core/theme.dart';
 import 'package:normaldo_gaming/ui/widgets/bouncing_button.dart';
 
 class BidWidget extends StatefulWidget {
@@ -18,20 +18,20 @@ class BidWidget extends StatefulWidget {
 }
 
 class _BidWidgetState extends State<BidWidget> {
-  static const bidStep = 5;
+  static const minBidStep = 5;
 
   late int _currentBid;
 
-  void _onMinusPressed() {
+  void _onMinusPressed(int step) {
     setState(() {
-      _currentBid -= bidStep;
+      _currentBid -= step;
     });
     widget.onBidChanged(_currentBid);
   }
 
-  void _onPlusPressed() {
+  void _onPlusPressed(int step) {
     setState(() {
-      _currentBid += bidStep;
+      _currentBid += step;
     });
     widget.onBidChanged(_currentBid);
   }
@@ -41,16 +41,20 @@ class _BidWidgetState extends State<BidWidget> {
     super.initState();
 
     final dollars = context.read<UserCubit>().state.user.dollars;
-    if (dollars < 5) {
+    if (dollars < minBidStep) {
       _currentBid = 0;
     } else {
-      _currentBid = 5;
+      _currentBid = minBidStep;
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final increaseStyle =
+        textTheme.displaySmall?.copyWith(color: NGTheme.green1);
+    final decreaseStyle =
+        textTheme.displaySmall?.copyWith(color: Colors.red[400]);
     return BlocConsumer<UserCubit, UserState>(
       listener: (context, state) {
         if (state.user.dollars < _currentBid) {
@@ -58,53 +62,122 @@ class _BidWidgetState extends State<BidWidget> {
           widget.onBidChanged(_currentBid);
         }
       },
-      builder: (context, state) => Column(
+      builder: (context, state) => Row(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Text('Bid'.tr(), style: textTheme.displayLarge),
-          const SizedBox(height: 8),
+          _decreaseBidderWrapper(
+            step: 100,
+            child: BouncingButton(
+              onPressed: () => _onMinusPressed(100),
+              child: Text(
+                '-100',
+                style: decreaseStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          _decreaseBidderWrapper(
+            step: 50,
+            child: BouncingButton(
+              onPressed: () => _onMinusPressed(50),
+              child: Text(
+                '-50',
+                style: decreaseStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          _decreaseBidderWrapper(
+            step: 5,
+            child: BouncingButton(
+              onPressed: () => _onMinusPressed(5),
+              child: Text(
+                '-5',
+                style: decreaseStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
           SizedBox(
-            width: 160,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Expanded(
-                  child: Visibility(
-                    visible: _currentBid > 5,
-                    child: BouncingButton(
-                      onPressed: _onMinusPressed,
-                      child: Text(
-                        '-',
-                        style: textTheme.displayLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-                Expanded(
-                    child: Text(
-                  _currentBid.toString(),
-                  style: textTheme.displayLarge,
-                  textAlign: TextAlign.center,
-                )),
-                Expanded(
-                  child: Visibility(
-                    visible: _currentBid < 100 &&
-                        state.user.dollars > _currentBid + bidStep,
-                    child: BouncingButton(
-                      onPressed: _onPlusPressed,
-                      child: Text(
-                        '+',
-                        style: textTheme.displayLarge,
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
+            width: 70,
+            child: Text(
+              _currentBid.toString(),
+              style: textTheme.displayLarge,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(width: 16),
+          _increaseBidderWrapper(
+            step: 5,
+            userDollars: state.user.dollars,
+            child: BouncingButton(
+              onPressed: () => _onPlusPressed(5),
+              child: Text(
+                '+5',
+                style: increaseStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          _increaseBidderWrapper(
+            step: 50,
+            userDollars: state.user.dollars,
+            child: BouncingButton(
+              onPressed: () => _onPlusPressed(50),
+              child: Text(
+                '+50',
+                style: increaseStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          const SizedBox(width: 8),
+          _increaseBidderWrapper(
+            step: 100,
+            userDollars: state.user.dollars,
+            child: BouncingButton(
+              onPressed: () => _onPlusPressed(100),
+              child: Text(
+                '+100',
+                style: increaseStyle,
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _decreaseBidderWrapper({required int step, required Widget child}) {
+    return AnimatedOpacity(
+      opacity: _currentBid > step ? 1 : 0.3,
+      duration: const Duration(milliseconds: 300),
+      child: IgnorePointer(
+        ignoring: _currentBid < step,
+        child: child,
+      ),
+    );
+  }
+
+  Widget _increaseBidderWrapper({
+    required int step,
+    required int userDollars,
+    required Widget child,
+  }) {
+    return AnimatedOpacity(
+      opacity: _currentBid + step <= 999 && userDollars > _currentBid + step
+          ? 1
+          : 0.3,
+      duration: const Duration(milliseconds: 300),
+      child: IgnorePointer(
+        ignoring:
+            !(_currentBid + step <= 999 && userDollars > _currentBid + step),
+        child: child,
       ),
     );
   }
