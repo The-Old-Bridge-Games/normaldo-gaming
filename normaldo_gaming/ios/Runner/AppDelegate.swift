3dc0f1b764b027ec3c5199a5ef5b29c6da4893e5
Sovetsk1y <39431565+Sovetsk1y@users.noreply.github.com>
2024-05-15 17:07:28 +0300

    prevent bottom edge ios native swipe up

--- normaldo_gaming/ios/Runner/AppDelegate.swift
+++ normaldo_gaming/ios/Runner/AppDelegate.swift
@@ -1,13 +1,29 @@
 import UIKit
 import Flutter
 
+@objc class DefferingSystemGesturesFlutterController: FlutterViewController {
+    override var prefersHomeIndicatorAutoHidden: Bool {
+      return false
+    }
+
+    override var preferredScreenEdgesDeferringSystemGestures: UIRectEdge {
+      return UIRectEdge.bottom
+    }
+}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    var flutterViewController: FlutterViewController?
+    
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+//      self.flutterViewController = DefferingSystemGesturesFlutterController()
+//      self.window = UIWindow.init(frame: UIScreen.main.bounds)
+//      self.window.rootViewController = UINavigationController.init(rootViewController: self.flutterViewController!)
+//      self.window.makeKeyAndVisible()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
