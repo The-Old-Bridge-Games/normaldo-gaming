237ded41ed5c0266bf39628b0a1655e55fa7e3ba
Sovetsk1y <39431565+Sovetsk1y@users.noreply.github.com>
2024-09-24 14:25:49 +0300

    upgraded libs and version

--- normaldo_gaming/lib/data/core/interceptors/auth_interceptor.dart
+++ normaldo_gaming/lib/data/core/interceptors/auth_interceptor.dart
@@ -5,13 +5,15 @@ import 'package:chopper/chopper.dart';
 import 'package:normaldo_gaming/core/errors.dart';
 import 'package:normaldo_gaming/data/auth/services/auth_local_storage.dart';
 
-class AuthInterceptor implements ResponseInterceptor {
+class AuthInterceptor implements Interceptor {
   const AuthInterceptor(this._authLocalStorage);
 
   final AuthLocalStorage _authLocalStorage;
 
   @override
-  FutureOr<Response> onResponse(Response response) {
+  FutureOr<Response<BodyType>> intercept<BodyType>(
+      Chain<BodyType> chain) async {
+    final response = await chain.proceed(chain.request);
     if (!response.isSuccessful) {
       if (response.statusCode == HttpStatus.unauthorized) {
         _authLocalStorage.deleteToken();
--- normaldo_gaming/lib/data/core/interceptors/token_interceptor.dart
+++ normaldo_gaming/lib/data/core/interceptors/token_interceptor.dart
@@ -4,19 +4,23 @@ import 'dart:io';
 import 'package:chopper/chopper.dart';
 import 'package:normaldo_gaming/data/auth/services/auth_local_storage.dart';
 
-class TokenInterceptor implements RequestInterceptor {
+class TokenInterceptor implements Interceptor {
   const TokenInterceptor(this._authLocalStorage);
 
   final AuthLocalStorage _authLocalStorage;
+
   @override
-  FutureOr<Request> onRequest(Request request) async {
+  FutureOr<Response<BodyType>> intercept<BodyType>(
+      Chain<BodyType> chain) async {
+    final request = chain.request;
     if (request.headers['needAuth'] == 'true') {
       final token = await _authLocalStorage.fetchToken();
-      if (token == null) return request;
-      return request.copyWith(
+      if (token == null) return chain.proceed(request);
+      final newRequest = request.copyWith(
           headers: request.headers
             ..[HttpHeaders.authorizationHeader] = 'Bearer $token');
+      return chain.proceed(newRequest);
     }
-    return request;
+    return chain.proceed(request);
   }
 }
